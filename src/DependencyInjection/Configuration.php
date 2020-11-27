0b7e8539243dc3907fd913607689658af0483938
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2020-11-27 15:34:55 +0100

    update composer.json and configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -40,7 +40,41 @@ final class Configuration implements ConfigurationInterface
                 ->arrayNode('options')
                     ->prototype('variable')->end()
                     ->ignoreExtraKeys(false)
-                    ->defaultValue(array())
+                    ->defaultValue(array(
+                        'type'             => 'notice',
+                        'title'            => false,
+                        'titleTrusted'     => false,
+                        'text'             => false,
+                        'textTrusted'      => false,
+                        'styling'          => 'brighttheme',
+                        'icons'            => 'brighttheme',
+                        'mode'             => 'no-preference',
+                        'addClass'         => '',
+                        'addModalClass'    => '',
+                        'addModelessClass' => '',
+                        'autoOpen'         => true,
+                        'width'            => '360px',
+                        'minHeight'        => '16px',
+                        'maxTextHeight'    => '200px',
+                        'icon'             => true,
+                        'animation'        => 'fade',
+                        'animateSpeed'     => 'normal',
+                        'shadow'           => true,
+                        'hide'             => true,
+                        'delay'            => 5000,
+                        'mouseReset'       => true,
+                        'closer'           => true,
+                        'closerHover'      => true,
+                        'sticker'          => true,
+                        'stickerHover'     => true,
+                        'labels'           => array(
+                            'close'   => 'Close',
+                            'stick'   => 'Pin',
+                            'unstick' => 'Unpin'
+                        ),
+                        'remove'           => true,
+                        'destroy'          => true,
+                    ))
                 ->end()
             ->end()
         ;
